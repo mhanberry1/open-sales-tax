114d1aaca597ede65ad958386dae0ca7aa6babbd
rohitarunachalam <71034798+rohitarunachalam@users.noreply.github.com>
2021-07-07 20:28:21 -0400

    Majority of Georgia Zip-code Functionality
    Simple JSON-based API using Node JS

---
+++ Node.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+const express = require("express");
+const cors = require('cors');
+   
+// json file with the data
+var data = fs.readFileSync('taxcode.json');
+   
+var elements = JSON.parse(data);
+const app = express();
+
+    
+app.listen(process.env.PORT, 
+    () => console.log("Server Start at the Port " + process.env.PORT));
+    
+app.use(express.static('public'));
+app.use(cors());
+  
+// when get request is made, alldata() is called
+app.get('/elements', alldata);
+   
+function alldata(request, response) {
+       
+    // Returns all information about the elements
+    response.send(elements);
+}
+  
+app.get('/elements/:element/', searchElement);
+  
+function searchElement(request, response) {
+    var word = request.params.element;
+       
+    if(elements[word]) {
+        var reply = { taxRate: elements[word] }    
+    }
+    else {
+        var reply = {
+            status:"404 Not Found"
+        }
+    }
+       
+    response.send(reply);
+}
